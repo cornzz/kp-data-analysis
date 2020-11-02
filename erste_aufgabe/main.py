1bf9939e01e3cf1b24d76d894f4319cd212e2bcf
cornzz <39997278+cornzz@users.noreply.github.com>
2020-11-02 22:11:14 +0100

    erste_aufgabe - cleaner method for hourly average

--- erste_aufgabe/main.py
+++ erste_aufgabe/main.py
@@ -1,6 +1,7 @@
+import requests
 import sqlite3 as sql
 import pandas as pd
-import requests
+import numpy as np
 from requests.auth import HTTPBasicAuth
 from zipfile import ZipFile
 
@@ -16,36 +17,43 @@ def download_data(refresh_old_data):
     # Download new data
     url_new = 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/air_temperature/recent/10minutenwerte_TU_01048_akt.zip'
     response = requests.get(url_new)
-    with open('data.zip', 'wb') as f:
+    with open('new_data.zip', 'wb') as f:
         f.write(response.content)
-    with ZipFile('data.zip', 'r') as z:
+    with ZipFile('new_data.zip', 'r') as z:
         files = z.namelist()
-        open('data.txt', 'wb').write(z.read(files[0]))
+        open('new_data.txt', 'wb').write(z.read(files[0]))
 
 
 def main():
     download_data(True)
 
-    conn = sql.connect('the.db')
-    # Create dataframe with renamed columns
-    df = pd.read_csv(open('data.txt'), delimiter=";", header=0, names=['id', 'timestamp', 'qn', 'airpressure',
-                                                                          'temperature', 'temperature_ground',
-                                                                          'humidity', 'temperature_dew', 'eor'])
+    # Create dataframe from new data with renamed columns
+    df = pd.read_csv(open('new_data.txt'),
+                     delimiter=";",
+                     header=0,
+                     names=['id', 'timestamp', 'qn', 'airpressure', 'temperature', 'temperature_ground', 'humidity',
+                            'temperature_dew', 'eor'])
     # Drop irrelevant data
     df = df.query("timestamp > 202010131050 & timestamp < 202010232400")\
            .drop(['qn', 'eor'], axis=1)
     # Append dataframe to existing table
+    conn = sql.connect('the.db')
     df.to_sql('dwd', conn, if_exists='append', index=False)
+
     # Export table 'dwd' as json
     df = pd.read_sql('SELECT * FROM dwd', conn)
-    with open('data.json.txt', 'w') as f:
+    with open('db_updated.json.txt', 'w') as f:
         df.to_json(f)
 
-    avg_df = pd.DataFrame(columns=['timestamp', 'average_temperature'])
-    for i in range(len(df) // 6):
-        rows = df['temperature'].iloc[i*6:i*6+6]
-        avg_df.loc[i] = [df['timestamp'][6 * i], rows.mean()]
+    # Create dataframe with every 6th timestamp
+    times = df[['timestamp']].iloc[::6].reset_index(drop=True)
+    # Create dataframe with average of every 6 temperature values
+    temps = df[['temperature']].groupby(np.arange(len(df)) // 6).mean()
+    # Concatenate columns and create new table 'hourly_avg'
+    avg_df = pd.concat([times, temps], axis=1)
     avg_df.to_sql('hourly_avg', conn, if_exists='replace', index=False)
+
+    conn.close()
 
 
 if __name__ == '__main__':
