28539cc3e8294d607d9f6cc7e0f92ddae1fe9776
cornzz <39997278+cornzz@users.noreply.github.com>
2021-01-18 16:27:43 +0100

    zehnte_aufgabe - add task 4

--- 10_performance_optimization/zehnte_aufgabe.py
+++ 10_performance_optimization/zehnte_aufgabe.py
@@ -8,30 +8,38 @@ def dist(p1, p2):
     return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
 
-def task1():
-    setup = """from __main__ import dist, data1, data2;\
-    import math"""
+def task1(n):
     code = '[dist(p1, p2) for p1 in data1 for p2 in data2]'
-    return timeit.timeit(code, setup, number=1)
+    return timeit.timeit(code, number=n, globals=globals()) / n
 
 
-def task2():
-    setup = """from __main__ import dist, data1, data2;\
-    import multiprocessing as mp;\
+def task2(n):
+    setup = """import multiprocessing as mp;\
     p = mp.Pool(mp.cpu_count())"""
     code = 'p.starmap(dist, [(p1, p2) for p1 in data1 for p2 in data2])'
-    return timeit.timeit(code, setup, number=1)
+    return timeit.timeit(code, setup, number=n, globals=globals()) / n
 
 
-def task3():
-    return timeit.timeit('euclid_c(data1, data2)', 'from __main__ import data1, data2; from euclid import euclid_c', number=1)
+def task3(n):
+    setup = 'from euclid import euclid_c'
+    code = 'euclid_c(data1, data2)'
+    return timeit.timeit(code, setup, number=n, globals=globals()) / n
+
+
+def task4(n):
+    setup = """d1 = np.repeat(data1, len(data2), axis=0);\
+    d2 = np.tile(data2, (len(data1), 1))"""
+    code = 'np.sqrt(np.sum((d1 - d2) ** 2, axis=1))'
+    return timeit.timeit(code, setup, number=n, globals=globals()) / n
 
 
 data1 = np.random.rand(2000, 2).tolist()
 data2 = np.random.rand(2000, 2).tolist()
+iterations = 10
 
 if __name__ == '__main__':
-    print(f'Task 1 - run time euclid: {task1()} s')
-    print(f'Task 2 - run time euclid parallel: {task2()} s')
-    print(f'Task 3 - run time euclid C: {task3()} s')
+    print(f'Task 1 - run time regular: \t {task1(iterations):.4f} s')       # ~ 1.29 s
+    print(f'Task 2 - run time parallel: \t {task2(iterations):.4f} s')      # ~ 1.70 s
+    print(f'Task 3 - run time C: \t\t {task3(iterations):.4f} s')           # ~ 0.70 s
+    print(f'Task 4 - run time vectorized: \t {task4(iterations):.4f} s')    # ~ 0.17 s
 
